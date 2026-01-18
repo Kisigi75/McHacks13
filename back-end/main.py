62cf592284a4bc63dfb12d92edf1f6418837d079
Rowan-Cp <rowanelizabeth.cp@gmail.com>
2026-01-18 00:26:12 -0500

    idk

--- back-end/main.py
+++ back-end/main.py
@@ -7,14 +7,19 @@ app = FastAPI()
 def get_employees():
     with conn.cursor() as cur:
         cur.execute("""
-            SELECT id, first_name, last_name
+            SELECT id, first_name, last_name, department
             FROM employees
             ORDER BY id
         """)
         rows = cur.fetchall()
 
     return [
-        {"id": r[0], "first_name": r[1], "last_name": r[2]}
+        {
+            "id": r[0],
+            "first_name": r[1],
+            "last_name": r[2],
+            "department": r[3]
+        }
         for r in rows
     ]
 
